a1eeef4284d844324eb39c557d28e7fada178bb2
AndersonJz <afjivara@gmail.com>
2019-02-25 20:51:49 -0500

    Fix script 3 Commit

--- server/database.js
+++ server/database.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 
-const URI = 'mongodb://localhost/crud'
+const URI = 'mongodb+srv://kido:speed@cluster0-b2bso.mongodb.net/test?retryWrites=true';
 
-mongoose.connect(URI)
+
+const client = new MongoClient(URI, { useNewUrlParser: true });
+client.connect(err => {
+    const collection = client.db("pam").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+MongoClient.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
-module.exports = mongoose;
+module.exports = MongoClient;
