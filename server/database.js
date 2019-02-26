2cba214c266b2c03d85265d2ac95f3c8e84996e6
AndersonJz <afjivara@gmail.com>
2019-02-25 22:28:00 -0500

    Fix script 4 Commit

--- server/database.js
+++ server/database.js
@@ -1,11 +1,20 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require('mongoose');
+const URI = 'mongodb+srv://kido:speed@db-jjmjv.mongodb.net/test?retryWrites=true';
 
-const URI = 'mongodb+srv://kido:speed@cluster0-b2bso.mongodb.net/test?retryWrites=true';
+mongoose.connect(URI)
+    .then(db => console.log('db is connected'))
+    .catch(err => console.error(err));
+
+module.exports = mongoose;
+
+/*const MongoClient = require('mongodb').MongoClient;
+
+const URI = 'mongodb+srv://kido:speed@db-jjmjv.mongodb.net/admin';
 
 
 const client = new MongoClient(URI, { useNewUrlParser: true });
 client.connect(err => {
-    const collection = client.db("pam").collection("devices");
+    const collection = client.db("test").collection("devices");
     // perform actions on the collection object
     client.close();
 });
@@ -15,4 +24,4 @@ MongoClient.connect(URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
-module.exports = MongoClient;
+module.exports = MongoClient; */
